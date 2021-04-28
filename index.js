e6e70594e92a8a72bc1f13f3798b8006ac8e0981
bwesterg <benton.westergaard@gmail.com>
2021-04-28 15:35:14 -0600

    refactors urls and functions

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
+const baseURL = "http://localhost:3000"
+const pokemonsURL = `${baseURL}/pokemons`
+
+
 const pokemonContainer = document.querySelector('.pokemon-container');
 
-fetch("http://localhost:3000/pokemons")
-    .then(response => response.json())
-    .then(pokemons => displayPokemons(pokemons))
-
+fetch(pokemonsURL)
+    .then(parseJSON)
+    .then(displayPokemons);
 
 function displayPokemons(pokemons) {
-    console.log('display pokemons')
-    pokemons.forEach(pokemon => showPokemon(pokemon));
+    pokemons.forEach(showPokemon);
+    removeLoadingGif();
+}
+
+function removeLoadingGif() {
+    const loading = document.querySelector('.loading');
+    loading.remove();
 }
 
 function showPokemon(pokemon) {
-    const pokemonCard = document.createElement('div');
-    pokemonCard.classList.add('pokemon-card');
-
-    const pokemonName = document.createElement('h2');
-    pokemonName.textContent = pokemon.name;
-    
-    const pokemonImage = document.createElement('img');
-    pokemonImage.src = pokemon.sprites.other["official-artwork"].front_default
+    const pokemonCard = createPokemonCard();
+    const pokemonName = createPokemonName(pokemon);
+    const pokemonImage = createPokemonImage(pokemon);
 
     pokemonCard.append(pokemonName, pokemonImage);
     pokemonContainer.append(pokemonCard);
+}
+
+function createPokemonCard() {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('pokemon-card');
+    return pokemonCard;
+}
+
+function createPokemonName({ name }) {
+    const pokemonName = document.createElement('h2');
+    pokemonName.textContent = name;
+    return pokemonName;
+}
+
+function createPokemonImage({ sprites }) {
+    const pokemonImage = document.createElement('img');
+    pokemonImage.src = sprites.other["official-artwork"].front_default
+    return pokemonImage;
+}
+
+function parseJSON(response) {
+    return response.json();
 }
